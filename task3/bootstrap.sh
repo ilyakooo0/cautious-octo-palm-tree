09cc06368908afb0206d2191f21953bd66beaba0
iko <ilyakooo0@gmail.com>
2021-10-28 20:49:49 +0300

    fixed everything

--- task3/bootstrap.sh
+++ task3/bootstrap.sh
@@ -2,10 +2,14 @@
 
 apt-get update
 apt-get install -y apache2
+
 service apache2 start
 
 sudo a2enmod cgid
 
 cp /vagrant/apache2.conf /etc/apache2/apache2.conf
 
-apachectl -k graceful
+sudo chmod +x /vagrant/dynamic/server.py
+
+service apache2 stop
+service apache2 start
