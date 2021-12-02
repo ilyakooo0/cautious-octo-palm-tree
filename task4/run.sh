3a7958155501335547b33fb05c804a99136202bd
iko <ilyakooo0@gmail.com>
2021-12-02 23:00:53 +0300

    removed cat

--- task4/run.sh
+++ task4/run.sh
@@ -4,7 +4,5 @@ mysql -h db -P 3306 -u root --password=admin --execute "CREATE DATABASE dostavim
 
 mysql -h db -P 3306 -u root --password=admin --database=dostavim </CREATE.sql
 
-cat /CREATE.sql
-
 java -jar /seed.jar
 java -jar /run.jar
